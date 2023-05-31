3d3a615b7d7533e21009b43934afe347fd4c685b
shree <nithyashree_sekhar@students.edu.sg>
2023-05-31 11:08:15 +0800

    Initial Commit

---
+++ githubtesting/githubtestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubtestingApp.swift
+//  githubtesting
+//
+//  Created by T Krobot on 5/31/23.
+//
+
+import SwiftUI
+
+@main
+struct githubtestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
